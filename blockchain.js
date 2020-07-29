4cbcb95b416b5fbbd20e7fa382527ea14d4f48a1
BrunoHellmeister <67346964+BrunoHellmeister@users.noreply.github.com>
2020-07-29 20:38:15 -0300

    Add files via upload

--- blockchain.js
+++ blockchain.js
@@ -18,7 +18,7 @@ function registrarRequerente() {
   }
 
   contratoComSignatario
-    .registrarRequerente($("#_enderecoRequerente").val()
+    .registrarRequerente($("#_enderecoRequerente").val())
     .then((transacao) => {
       $("#descricaoStatusTransacoes").html("Transação enviada. Aguarde pela mineração...");
       $("#statusTransacoes").toggle();
@@ -186,13 +186,7 @@ function retirarRequerida() {
 
 
 //rascunho
-  var valorVenal = $("#_valorVenal").val() * 1;
-    if (valorVenal < 10000) {
-    alert("Valor venal inválido");
-    $("#_valorVenal").focus();
-    return;
-  }
-
+  
 function enviaVoto(_opcaoDesejada) {
     $("#descricaoStatusTransacoes").html("Transação enviada. Aguarde pela mineração...");
     $("#statusTransacoes").show();
@@ -251,3 +245,63 @@ function enviaVoto(_opcaoDesejada) {
     });
 }
   
+
+function salvarRegistro() {
+  event.preventDefault();
+  if ($("#_endereco").val().length != 42) {
+    $("#_endereco").focus();
+    alert("Endereço inválido");
+    return;
+  }
+
+  if (!$("#_endereco").val().startsWith("0x")) {
+    alert("Endereço inválido");
+    $("#_endereco").focus();
+    return;
+  }
+
+  if ($("#_nomeProprietario").val().length < 5) {
+    alert("Nome do proprietário inválido");
+    $("#_nomeProprietario").focus();
+    return;
+  }
+
+  var valorVenal = $("#_valorVenal").val() * 1;
+  if (valorVenal < 10000) {
+    alert("Valor venal inválido");
+    $("#_valorVenal").focus();
+    return;
+  }
+
+  if (typeof contratoComSignatario === "undefined") {
+    alert("Você não está conectado ao Ethereum. Verifique seu Metamask");
+    return;
+  }
+
+  contratoComSignatario
+    .registraImovel($("#_endereco").val(), $("#_nomeProprietario").val(), $("#_valorVenal").val() * 1)
+    .then((transacao) => {
+      $("#descricaoStatusTransacoes").html("Transação enviada. Aguarde pela mineração...");
+      $("#statusTransacoes").toggle();
+      transacao
+        .wait()
+        .then((resultado) => {
+          console.log("registraImovel - o resultado foi ", resultado);
+          if (resultado.status === 1) {
+            $("#descricaoStatusTransacoes").html("Transação executada.");
+          } else {
+            $("#descricaoStatusTransacoes").html("Houve um erro na execução da transação no Ethereum.");
+          }
+        })
+        .catch((err) => {
+          console.error("registraImovel - a transação foi minerada e houve um erro. Veja abaixo");
+          console.error(err);
+          $("#descricaoStatusTransacoes").html("Algo saiu errado: " + err.message);
+        });
+    })
+    .catch((err) => {
+      console.error("registraImovel - tx só foi enviada");
+      console.error(err);
+      $("#descricaoStatusTransacoes").html("Algo saiu errado antes de enviar ao Ethereum: " + err.message);
+    });
+}
